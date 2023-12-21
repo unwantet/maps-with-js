6381c3eaf0c26c5f78f943c654f5ead649ca130a
unwantet <darkblezer59@gmail.com>
2023-12-21 21:36:13 +0500

    ref

--- app.js
+++ app.js
@@ -2,15 +2,33 @@ const inputEl = document.querySelector("input")
 const form = document.querySelector("form")
 const ipEl = document.querySelector(".ip-a")
 
-ymaps.ready(renderMap);
+ymaps.ready(init);
 
-function renderMap() {
-    let myMap = new ymaps.Map("map",{
-        center: [40.388100149508716, 71.78784333878554],
-        zoom: 18
-    })
-}
+    function init() {
+        var myMap = new ymaps.Map("map", {
+            center: [41.3111, 69.2797], // Toshkent markazi koordinatalari
+            zoom: 12
+        });
 
+        var searchControl = new ymaps.control.SearchControl({
+            options: {
+                provider: 'yandex#search'
+            }
+        });
+
+        myMap.controls.add(searchControl);
+
+        document.getElementById('searchForm').addEventListener('submit', function (e) {
+            e.preventDefault();
+            var inputValue = document.getElementById('searchInput').value;
+
+            searchControl.search(inputValue).then(function (res) {
+                var coordinates = res.geoObjects.get(0).geometry.getCoordinates();
+                myMap.geoObjects.removeAll();
+                myMap.geoObjects.add(new ymaps.Placemark(coordinates));
+            });
+        });
+    }
 form.addEventListener("submit", e =>{
     e.preventDefault();
     ipEl.textContent = inputEl.value
